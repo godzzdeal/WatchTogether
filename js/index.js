a3a70aeff15b1c7d7005c31c0a1b12f5a649945a
godzzdeal <danrs1980@gmail.com>
2023-03-04 10:50:19 +0300

    fixed and upd index, add env

--- js/index.js
+++ js/index.js
@@ -1,22 +1,10 @@
-let socket = io.connect();
-let root = 'root_primary_room';
-let room = 'beta_primary_room';
-let version;
 
-socket.on('connect', function () {
-  console.log('Client connect socket');
-  socket.emit('joinRoom', { un: root, rm: room }, function (err) {
-    if (err) {
-      alert(err);
-      window.location.href = '/';
-    }
-  });
+var btn_signin = document.getElementById("signin");
 
-  socket.on('validate', function (data) {
-    console.log('Validation');
-    document.getElementsByName('version_footer')[0].innerHTML = data.version;
-    document.getElementsByName('version_header')[0].innerHTML = data.version;
-    document.getElementsByName('author_footer')[0].innerHTML = data.author;
-  });
+btn_signin.addEventListener("click", (e) => {
+  // prevent the form from submitting
+  e.preventDefault();
+  
+  window.location.href='signin';
+
 });
-
--- js/lib/db.js
+++ js/lib/db.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'node-jwt'
+  host: process.env.HOST_DB,
+  port: process.env.PORT_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_DB
 });
 
 connection.connect(function (err) {

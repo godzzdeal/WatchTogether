bf4f3f23c5a5785a84d9105bd27addf363df2417
godzzdeal <danrs1980@gmail.com>
2023-03-04 11:20:02 +0300

    upd old room in

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 
 var btn_signin = document.getElementById("signin");
+var btn_start_watch = document.getElementById("start_watch");
 
 btn_signin.addEventListener("click", (e) => {
   // prevent the form from submitting
   e.preventDefault();
   
   window.location.href='signin';
+
+});
+
+btn_start_watch.addEventListener("click", (e) => {
+  // prevent the form from submitting
+  e.preventDefault();
+  
+  window.location.href='old_room_in';
 
 });
